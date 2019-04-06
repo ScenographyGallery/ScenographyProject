a3c29ba137b2efb650b9b09aeb1477548f6153b6
Vadym Vasko <vadym.vasko@kindgeek.com>
2019-04-06 16:06:10 +0300

    vv_home: init

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AuthComponent } from './home/auth/auth.component';
+import {HomeComponent} from './home/home.component';
+import { HeaderComponent } from './home/header/header.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    AuthComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
