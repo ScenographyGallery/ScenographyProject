37640a105d4a60e6cbb9ac903a94ea518f44a7c3
yavorco <roman.yavoriv@yahoo.com>
2019-04-07 12:27:42 +0300

    Add json-server

--- client/src/app/artists/services/artist-data.service.ts
+++ client/src/app/artists/services/artist-data.service.ts
@@ -1,48 +1,24 @@
-import { Project } from './../../models/project';
+import { HttpClient } from '@angular/common/http';
 import { Artist } from './../../models/artist';
 import { Injectable } from '@angular/core';
-import { of, Observable } from 'rxjs';
-
-const PROJECTS_MOCK: Project[] = [
-  { name: 'Project 1', image: 'https://www.w3schools.com/w3images/fjords.jpg',},
-  { name: 'Project 2', image: 'https://www.w3schools.com/w3images/fjords.jpg',},
-  { name: 'Project 3', image: 'https://www.w3schools.com/w3images/fjords.jpg',},
-  { name: 'Project 4', image: 'https://www.w3schools.com/w3images/fjords.jpg',}
-]
-
-const ARTISTS_MOCK = [
-  { 
-    id: '1',
-    description: "Scenograph Lesya Ukrainka National Academic Theater Lviv",
-    firstName: 'Roman', 
-    lastName: 'Yavoriv', 
-    tags: ['light', 'beauty', 'flovers'],
-    projects: PROJECTS_MOCK,
-    avatar: 'https://pbs.twimg.com/profile_images/1047668019453972480/a0SAn64i_400x400.jpg',
-    cover: 'https://images.unsplash.com/photo-1554384919-3c159e9023be?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=975&q=80' },
-  { id: '1', firstName: 'Roman', lastName: 'Yavoriv', cover: 'https://images.unsplash.com/photo-1554384919-3c159e9023be?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=975&q=80' },
-  { id: '1', firstName: 'Roman', lastName: 'Yavoriv', cover: 'https://images.unsplash.com/photo-1554384919-3c159e9023be?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=975&q=80' },
-  { id: '1', firstName: 'Roman', lastName: 'Yavoriv', cover: 'https://images.unsplash.com/photo-1554384919-3c159e9023be?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=975&q=80' },
-  { id: '1', firstName: 'Roman', lastName: 'Yavoriv', cover: 'https://images.unsplash.com/photo-1554384919-3c159e9023be?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=975&q=80' },
-  { id: '1', firstName: 'Roman', lastName: 'Yavoriv', cover: 'https://images.unsplash.com/photo-1554384919-3c159e9023be?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=975&q=80' },
-  { id: '1', firstName: 'Roman', lastName: 'Yavoriv', cover: 'https://images.unsplash.com/photo-1554384919-3c159e9023be?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=975&q=80' },
-  { id: '1', firstName: 'Roman', lastName: 'Yavoriv', cover: 'https://images.unsplash.com/photo-1554384919-3c159e9023be?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=975&q=80' },
-  { id: '1', firstName: 'Roman', lastName: 'Yavoriv', cover: 'https://images.unsplash.com/photo-1554384919-3c159e9023be?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=975&q=80' },
-  { id: '1', firstName: 'Roman', lastName: 'Yavoriv', cover: 'https://images.unsplash.com/photo-1554384919-3c159e9023be?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=975&q=80' },
-]
+import { Observable } from 'rxjs';
+import { environment } from 'src/environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ArtistDataService {
+  baseUrl = environment.baseUrl;
 
-  constructor() { }
+  constructor(
+    private http: HttpClient,
+  ) { }
 
   getArtists(): Observable<Artist[]> {
-    return of(ARTISTS_MOCK);
+    return this.http.get<Artist[]>(`${this.baseUrl}artists`);
   }
 
   getArtist(id: string): Observable<Artist> {
-    return of(ARTISTS_MOCK[0]);
+    return this.http.get<Artist>(`${this.baseUrl}artists/${id}`);
   }
 }
